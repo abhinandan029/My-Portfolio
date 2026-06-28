54bfbf60e73261fdd66785b5b7dc1e68776e9fcd
Abhinandan Manakapure <abhinandanaj29@gmail.com>
2026-06-28 20:24:44 +0530

    a

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Hashrouter>
+    <HashRouter>
       <App />
-    </Hashrouter>
+    </HashRouter>
   </StrictMode>,
 )
